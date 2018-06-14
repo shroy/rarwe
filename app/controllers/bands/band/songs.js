e6a1c483f2618ad0b01bcb8f09397fe3f1720bf0
Josh Shroy <joshshroy@gmail.com>
2018-06-13 23:42:51 -0700

    Finish - Components

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -22,6 +22,10 @@ export default Controller.extend({
       let newSong = Song.create({ title: this.newSongTitle });
       this.model.songs.pushObject(newSong)
       this.set('newSongTitle', '');
+    },
+
+    updateRating(song, rating) {
+      song.set('rating', song.rating === rating ? 0 : rating);
     }
   }
 });
